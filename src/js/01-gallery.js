d65e5bfafdcce51b4cddb6106196f2c46bfa89d5
Antonyly <antonlyzun@gmail.com>
2022-07-20 15:08:20 +0300

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = createGallery(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGallery(img) {
+  return img
+    .map(({ original, preview, description }) => {
+      return `
+        <a
+        class="gallery__item"
+        href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}" />
+        </a>
+    `;
+    })
+    .join("");
+};
+  
+new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 500,
+});
