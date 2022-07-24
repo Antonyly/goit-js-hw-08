dc61d5bb448399dc0d0c5b7d05bb7fdda5df2036
Antonyly <antonlyzun@gmail.com>
2022-07-24 14:09:33 +0300

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,8 +6,13 @@ const iframeEl = document.querySelector('iframe');
 const playerEl = new Vimeo(iframeEl);
 
 
+const onPlayEl = ({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+// playerEl.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+playerEl.setCurrentTime(0).then(function () {
+    localStorage.getItem('videoplayer-current-time')
+});
+
 playerEl.on('timeupdate', throttle(onPlayEl, 500));
-function onPlayEl({ seconds }) {
-    localStorage.setItem('videoplayer-current-time', seconds)
-}
-    playerEl.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,145 @@
 import throttle from 'lodash.throttle';
 
-const CONTACT_FORM_KEY = 'feedback-form-state'
-const contactFormEl = document.querySelector('.feedback-form');
-const formData = {};
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
 
-const fillContactFormElements = form => {
-    const formData = JSON.parse(localStorage.getItem(CONTACT_FORM_KEY));
-    const formElements = form.elements;
+let formData = {};
 
-    console.log(formData);
+form.addEventListener('input', inputValue);
+form.addEventListener('submit', onSubmitForm);
 
-    for (const key in formData) {
-        if (formData.hasOwnProperty(key)) {
-            formElements[key].value = formData[key];
-        }
-    }
-}
-fillContactFormElements(contactFormEl);
-
-const onContactFormChange = event => {
-    const { target } = event;
-
-    const contactFormValue = target.value;
-    const contactFormName = target.name;
-
-    formData[contactFormName] = contactFormValue;
-
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+function onFormData(el) {
+  const data = JSON.stringify(formData);
+    if(data) {
+    
+   return localStorage.setItem(el, data);
+  } 
 };
-const onContactFormSubmit = event => {
-  event.preventDefault();
 
-  localStorage.removeItem('feedback-form-state');
+function dataFromLocalStorage(el) {
+    const data = localStorage.getItem(el);
 
-  contactFormEl.reset();
+  if (data === null) {
+   return el = undefined;
+    
+  } else {
+return JSON.parse(data);
+  }
+};
 
-  console.log(formData);
+const objFromLocalStorage = dataFromLocalStorage('feedback-form-state');
+
+if (objFromLocalStorage) {
+  email.value = objFromLocalStorage.email;
+  message.value = objFromLocalStorage.message;
+  formData = objFromLocalStorage;
+} else {
+  email.value = '';
+  message.value = '';
 }
 
-contactFormEl.addEventListener('input', throttle(onContactFormChange, 1000));
-contactFormEl.addEventListener('submit', onContactFormSubmit);
+function onSubmitForm(e) {
+    e.preventDefault();
+        const formEl = e.currentTarget.elements;
+
+    const emailEl = formEl.email.value;
+    const messageEl = formEl.message.value;
+
+    if (emailEl === '' || messageEl === '') {
+        alert('Всі поля повинні бути заповнені');
+    } 
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+  console.log(formData);
+};
+
+function inputValue(e) {
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+
+  if (email.value || message.value) {
+    formData = {
+      email: email.value,
+      message: message.value,
+    };
+
+    throttle(onFormData, 500)('feedback-form-state', formData);
+  }
+};
+
+
+
+// import throttle from 'lodash.throttle';
+
+// const CONTACT_FORM_KEY = 'feedback-form-state';
+// const contactFormEl = document.querySelector('.feedback-form');
+// const formData = {};
+// function onFormData(el) {
+//   const data = JSON.stringify(formData);
+//     if(data) {
+    
+//    return localStorage.setItem(el, data);
+//   } 
+// };
+
+// function dataFromLocalStorage(el) {
+//     const data = localStorage.getItem(el);
+
+//   if (data === null) {
+//    return el = undefined;
+    
+//   } else {
+// return JSON.parse(data);
+//   }
+// };
+
+// const objFromLocalStorage = dataFromLocalStorage('feedback-form-state');
+
+// const fillContactFormElements = form => {
+//     const formData = JSON.parse(localStorage.getItem(CONTACT_FORM_KEY));
+//     const formElements = form.elements;
+
+//     console.log(formData);
+    
+//     for (const key in formData) {
+//         if (formData.hasOwnProperty(key)) {
+//             formElements[key].value = formData[key];
+//         }
+//     }
+// }
+// fillContactFormElements(contactFormEl);
+
+// const onContactFormChange = event => {
+//     const { target } = event;
+
+//     const contactFormValue = target.value;
+//     const contactFormName = target.name;
+
+//     formData[contactFormName] = `${contactFormValue}`;
+
+//     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+
+// };
+
+// const onContactFormSubmit = event => {
+//     event.preventDefault();
+
+//     localStorage.removeItem(CONTACT_FORM_KEY);
+
+
+    // const formEl = event.currentTarget.elements;
+
+    // const emailEl = formEl.email.value;
+    // const messageEl = formEl.message.value;
+
+    // if (emailEl === '' || messageEl === '') {
+    //     alert('Всі поля повинні бути заповнені');
+    // } 
+//         console.log(formData);
+//     contactFormEl.reset();
+    
+// }
+// contactFormEl.addEventListener('input', throttle(onContactFormChange, 1000));
+// contactFormEl.addEventListener('submit', onContactFormSubmit);
